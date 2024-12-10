0387f8a184a51c169b54d27edf5bd047788b993d
Cristian <fuentesrosascristian@gmail.com>
2024-12-10 02:42:53 -0600

    Frontend

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -38,7 +38,7 @@ export default function ProductDetails({product} : ProductDetailsProps){
 
                     <fetcher.Form method='POST'>
                         <button
-                            type='button'
+                            type='submit'
                             name='id'
                             value={product.id}
                             className={`${isAvailable ? 'text-black' : 'text-red-600'} rounded-lg p-2 text-xs uppercase font-bold w-full border border-black-100 hover:cursor-pointer`}
